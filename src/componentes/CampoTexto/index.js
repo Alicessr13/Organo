116eff06db40cf924b99af1732cd88a58950be61
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2025-01-02 17:13:51 -0300

    Inicio projeto

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+const CampoTexto = () => {
+    return(
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input></input>
+        </div>
+    )
+}
+
+export default CampoTexto;
