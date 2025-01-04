0b7080e1b969ca9860d2e24e08cb8c456c8c9310
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2025-01-03 21:51:17 -0300

    colaborador card

---
+++ src/componentes/Colaborador/index.jsx
@@ -0,0 +1,18 @@
+import './Colaboradores.css'
+
+const Colaborador = () =>{
+    return(
+        <div className='colaborador'>
+           <div className='cabecalho'>
+                <img src="https://github.com/Alicessr13.png" alt="Foto de perfil" />
+            </div> 
+
+            <div className='rodape'>
+                <h4>Alice</h4>
+                <h5>Programador</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador;
